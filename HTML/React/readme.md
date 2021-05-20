57dc4cc1e88fbec76cf7498998febc9852088e80
sts219 <sts219@ic.ac.uk>
2021-05-20 12:01:35 +0100

    Added React server files

---
+++ HTML/React/readme.md
@@ -0,0 +1,3 @@
+REACT
+=====
+To create a React app, run `npx create-react-app <app_name>`, which will create a directory with the app name that can be used to develop the web-app. Running `npm start` will start the developmental server, allowing you to view the webpage from your browser.
---
+++ HTML/React/test/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    state = { counter:0 }
+
+    incrementCounter = ()=> {
+        this.setState({counter:this.state.counter+1});
+    }
+
+
+    render(){
+        const colorStyle = {
+            color:this.props.color,
+            fontSize:this.props.size+"px",
+            backgroundColor:this.props.backgroundColor
+        }
+        return (
+            <div style={colorStyle}>
+                React Component
+                <br /><br />
+                <button onClick={this.incrementCounter}>Click Me!</button>
+                <br /><br />
+                {this.state.counter}
+            </div>
+        )
+    }
+}
+
+export default App;
