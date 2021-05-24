4b96616d93558d2cf160abeddff0a7c60d26b5c0
yv19 <yuna.valade19@imperial.ac.uk>
2021-05-24 18:24:54 +0100

    coordinates test

--- HTML/React/web-app/src/components/Home.js
+++ HTML/React/web-app/src/components/Home.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import {makeStyles} from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
 const useStyles = makeStyles((theme)=>({
     root:{
@@ -17,42 +18,56 @@ const useStyles = makeStyles((theme)=>({
 
 function Home(){
     const classes = useStyles();
+
     const [inputFields, setInputFields] = useState([
         {coordinateX: '', coordinateY: ''}
     ])
 
-    const handleChangeInput=(index,event)=>{
+    //Allows the user to input text, needs modification to only accept numbers
+    const handleChangeInput=(event)=>{
         const values=[...inputFields];
-        values[index][event.target.name]=event.target.value;
+        values[event.target.name]=event.target.value;
         setInputFields(values);
+        console.log(values);
+    }
+
+    const handleSubmit=(event)=>{
+        event.preventDefault();
+        const coordinates={
+            coordinateX: inputFields.coordinateX,
+            coordinateY: inputFields.coordinateY
+        }
+        axios.post('localhost:8080', coordinates)
+            .then(response=>{
+                //setInputFields(response.coordinates)
+                console.log(response.coordinates)
+        })
     }
 
     return(
         <Container>
             <h1> Home Page </h1>
             <h2>Coordinates</h2>
-            <form className={classes.root}>
-                { inputFields.map((inputField, index) => (
-                    <div key={index}>
+            <form className={classes.root} onSubmit={event=> handleSubmit(event)}>
+                    <div>
                     <TextField
                         name="coordinateX"
                         label="Coordinate X"
-                        value={inputField.coordinateX}
-                        onChange={event=> handleChangeInput(index,event)}
+                        value={inputFields.coordinateX}
+                        onChange={event=> handleChangeInput(event)}
                         />
                      <TextField
                         name="coordinateY"
                         label="Coordinate Y"
-                        value={inputField.coordinateY}
-                        onChange={event=> handleChangeInput(index,event)}
+                        value={inputFields.coordinateY}
+                        onChange={event=> handleChangeInput(event)}
                         />
                     </div>
-                ))}
                 <Button className={classes.button}
                  variant="contained" 
                  color="primary" 
                  type="submit">
-                    Submit
+                Submit
                 </Button>
             </form>
         </Container>
