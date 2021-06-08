87d725ed233c2a1f78b9a02e2f2feaededca7990
sts219 <sts219@ic.ac.uk>
2021-06-08 16:31:14 +0100

    Updated import files to prep for server boilerplate generation

--- Server/pathfinding/index.cpp
+++ Server/pathfinding/index.cpp
@@ -3,14 +3,14 @@
 #include "pathfinding.hpp"
 
 // native C++ function that is assigned to 'getPath' property on 'exports' object
-Napi::String getPath(const Napi::CallbackInfo& info) {
+Napi::Object getPath(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
 
     // input parameters
     std::vector<std::pair<int,int>> obstacles = {{1000,1450}, {2230,3100}, {2700,3600}, {3100,4450}, {3320,4550}};
 
     // return new 'Napi::String' value
-    return Napi::String::New(env, result);
+    return Napi::Oject::New(env, result);
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
@@ -18,7 +18,7 @@ Napi::Object Init(Napi::Env env, Napi::Object exports) {
     // set a key on 'exports' object
     exports.Set(
         Napi::String::New(env, "getPath"), // property name => "getPath"
-        Napi::FUnction::New(env, getPath) // property value => `getPath` function
+        Napi::Function::New(env, getPath) // property value => `getPath` function
     );
     
     // return exports object (always)
