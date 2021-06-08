7e01209b7efd15df63a8898c3e077f46e5d4407a
sts219 <sts219@ic.ac.uk>
2021-06-08 22:02:58 +0100

    I want to die

--- Server/pathfinding/index.cpp
+++ Server/pathfinding/index.cpp
@@ -6,11 +6,43 @@
 Napi::Object getPath(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
 
+    if (info.Length() < 3) {
+        Napi::TypeError::New(env, "Wrong number of arguments")
+            .ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    if ()
+
     // input parameters
-    std::vector<std::pair<int,int>> obstacles = {{1000,1450}, {2230,3100}, {2700,3600}, {3100,4450}, {3320,4550}};
+    std::pair<int,int> pos = info[0];
+    std::pair<int,int> dest = info[1];
+    std::vector<std::pair<int,int>> obstacles = info[2];
+
 
     // return new 'Napi::String' value
     return Napi::Oject::New(env, result);
+}
+
+Napi::Value Add(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+
+  if (info.Length() < 2) {
+    Napi::TypeError::New(env, "Wrong number of arguments")
+        .ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  if (!info[0].IsNumber() || !info[1].IsNumber()) {
+    Napi::TypeError::New(env, "Wrong arguments").ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  double arg0 = info[0].As<Napi::Number>().DoubleValue();
+  double arg1 = info[1].As<Napi::Number>().DoubleValue();
+  Napi::Number num = Napi::Number::New(env, arg0 + arg1);
+
+  return num;
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
---
+++ Server/test-addon/index.js
@@ -0,0 +1,5 @@
+const testAddon = require('./build/Release/testaddon.node');
+console.log('addon',testAddon);
+console.log('hello: ',testAddon.hello());
+console.log('add: ',testAddon.add(5, 10));
+module.exports = testAddon;
