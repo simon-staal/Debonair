b89d6bff67ceddd6d23625abeca119b6b9f0925a
sts219 <sts219@ic.ac.uk>
2021-06-11 17:00:06 +0100

    Removed testing folder

--- Server/test-addon/cppsrc/Samples/functionexample.h
+++
@@ -1,8 +0,0 @@
-#include <napi.h>
-
-namespace pathfinding {
-    std::string genPath(std::string pos, std::string dest, std::string obs);
-    Napi::String GenPathWrapped(const Napi::CallbackInfo &info);
-
-    Napi::Object Init(Napi::Env env, Napi::Object exports);
-}
