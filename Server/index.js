c35f42e0062f49092e80300f25bdf49cef70602b
sts219 <sts219@ic.ac.uk>
2021-06-12 11:59:33 +0100

    Updated REST API

--- Server/index.js
+++ Server/index.js
@@ -23,6 +23,7 @@ for (i; i < 10000; i++) {
 	total += (end-start);
 }
 console.log("Average time for 10000 iterations: "+(total/i)+"ms");
+//process.kill(process.pid, 'SIGTERM');
 */
 // ---------------- Admin shit -------------------
 // Setting up communication
@@ -295,8 +296,8 @@ app.post("/coords", (req,res) => {
 	for ( let i in obstacles ) {
 		if (obstacles[i].x) obstacles_string += `{${obstacles[i].x},${obstacles[i].y}}`;
 	}
-	console.log(`${parseInt(req.body.coordinateX, 10)},${parseInt(req.body.coordinateY,10)}`)
-	console.log("Obstacles: "+obstacles_string);
+	//console.log(`${parseInt(req.body.coordinateX, 10)},${parseInt(req.body.coordinateY,10)}`)
+	//console.log("Obstacles: "+obstacles_string);
 	path = JSON.parse(Pathfinder.genPath(`${rover.x},${rover.y}`,`${parseInt(req.body.coordinateX, 10)},${parseInt(req.body.coordinateY,10)}`,obstacles_string));
     console.log(JSON.stringify(path));
 	res.send(path);
