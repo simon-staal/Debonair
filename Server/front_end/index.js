cb5c1ed78b2192398fc877024df465706c553a31
sts219 <sts219@ic.ac.uk>
2021-06-13 16:27:19 +0100

    Finalised client changes

--- Server/front_end/index.js
+++ Server/front_end/index.js
@@ -9,26 +9,29 @@ let vals = [];
 function sendTest() {
     setTimeout(() => {
 			setInterval(() => {
+                if(i === 500) process.exit(0);
 				start = Date.now();
-				axios.get('https://debonair.duckdns.org:8443/', "Hello from frontend")
+				axios.get('https://debonair.duckdns.org:8443/')
                 .then(response=>{
                     end = Date.now();
+                    console.log("Received response")
                     total += (end-start);
                     vals.push(end-start);
                     i++;
-                    if(i === 50) {
-                        console.log("Average time for "+i+" runs = "+(total/i));
+                    if(i === 500) {
+                        console.log("Average time for "+i+" runs = "+(total/i)+"ms");
                         console.log(vals.toString());
                         fs.writeFile('./results.csv',vals.toString(), err => {
                             if(err) console.log(err)
                         });
+                        //process.exit(0);
                     }
                 })
                 .catch(err => {
                     console.log("Received error: " + err);
                     return;
                 })
-    			},500);
+    			},100);
 	}, 1000);
 }
 
