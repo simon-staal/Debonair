d82fc3f41aef00bcd9ea455aa1b8efb98a543ecc
sts219 <sts219@ic.ac.uk>
2021-06-15 23:49:08 +0000

    Not sure what this is (hopefully nothing breaks)

--- Server/index.js
+++ Server/index.js
@@ -227,7 +227,7 @@ client.on('message', (topic, message, packet) => {
 		rover.y = msg.y;
 		rover.angle = msg.a;
 		rover.lastUpdate = time.getTime();
-		//console.log("x: "+rover.x+" y: "+rover.y+" angle: "+rover.angle);
+		console.log("x: "+rover.x+" y: "+rover.y+" angle: "+rover.angle);
 	}
 	if (topic === "fromESP32/nextpoint") {
 		if (path.points) {
