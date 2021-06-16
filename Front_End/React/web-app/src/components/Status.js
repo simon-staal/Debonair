6fe2e35710f6001955a744a8806d13f7a433169c
yv19 <yuna.valade19@imperial.ac.uk>
2021-06-16 14:01:23 +0100

    batteryrequesteverySec

--- Front_End/React/web-app/src/components/Status.js
+++ Front_End/React/web-app/src/components/Status.js
@@ -152,7 +152,7 @@ const getBattery=()=>{
     
    const batterylevel=()=>{
          getBattery();
-         setTimeout(batterylevel,3000)
+         setTimeout(batterylevel,1000)
         }
     
     
